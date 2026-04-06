531ab3975a7c744cc2494405aa258f7a06620f6e
Denis <abdulbasiyrngithub@gmail.com>
2026-04-06 16:03:42 +0500

    feat: update AppError class, add other features

--- server/src/utils/app.error.js
+++ server/src/utils/app.error.js
@@ -1,7 +1,20 @@
 
 export class AppError extends Error {
-  constructor(message, statusCode) {
-    super(message)
-    this.statusCode = statusCode
+  constructor({
+    userMessage,
+    techMessaage  = '',
+    statusCode    = 500,
+    isOperational = true,
+    errorCode     = null
+  }) {
+    super(techMessaage || userMessage) 
+    this.userMessage   = userMessage 
+    this.techMessaage  = techMessaage 
+    this.statusCode    = statusCode 
+    this.isOperational = isOperational
+    this.errorCode     = errorCode
+    this.status        = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+
+    Error.captureStackTrace(this, this.constructor)
   }
 }
